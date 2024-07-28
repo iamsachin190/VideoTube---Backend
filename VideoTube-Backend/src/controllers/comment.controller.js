ac132b037b8af09a7bba55a377b327ca29abb2f6
Sachin Sadh <117935531+iamsachin190@users.noreply.github.com>
2024-07-29 00:16:23 +0530

    Update comment.controller.js

--- VideoTube-Backend/src/controllers/comment.controller.js
+++ VideoTube-Backend/src/controllers/comment.controller.js
@@ -11,21 +11,8 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 })
 
-const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
-})
-
-const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-})
-
-const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
-})
 
 export {
     getVideoComments, 
-    addComment, 
-    updateComment,
-     deleteComment
+   
     }
